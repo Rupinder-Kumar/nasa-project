85d1cd45a0a47f77892f8aef537cceb3a897d8d0
Rupinder <rupinder3972@gmail.com>
2023-08-19 16:00:48 +0530

    added changes for delete api and implemented same at client level

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,12 +6,17 @@ const launch = {
     rocket: 'Explore IS1',
     launchDate: new Date('Decement 27, 2030'),
     target: 'Kepler-442 b',
-    customer: ['NASA', 'ZTM'],
+    customers: ['NASA', 'ZTM'],
     upcoming: true,
     success: true,
 }
 
 launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
+
 
 function getAllLaunches() {
     return Array.from(launches.values());
@@ -21,13 +26,23 @@ function addNewLaunch(launch) {
     latestFlightNumber++;
     launches.set(latestFlightNumber, Object.assign(launch, {
         flightNumber: latestFlightNumber,
-        customer: ['ZTM','NASA'],
+        customers: ['ZTM','NASA'],
         upcoming: true,
         success: true,
     }));
 }
 
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
+}
+
+
 module.exports = {
+    existsLaunchWithId,
     getAllLaunches,
-    addNewLaunch
+    addNewLaunch,
+    abortLaunchById
 }
