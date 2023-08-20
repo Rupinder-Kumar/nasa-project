1fc2279355554d0603d63b5391c13020443c8fe1
Rupinder <rupinder3972@gmail.com>
2023-08-20 16:47:43 +0530

    created sync function for creating planets data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -11,6 +11,28 @@ function isHabitablePlanet(planet) {
         && planet['koi_prad'] < 1.6;
 }
 
+function loadPlanetsDataSync() {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true,
+        }))
+        .on('data', (data) => {
+            if (isHabitablePlanet(data)) {
+                habitablePlanets.push(data);
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+        })
+        .on('end', () => {
+            console.log(habitablePlanets.map((planet) => {
+                return planet['kepler_name'];
+            }));
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+        });
+}
+
 function loadPLanetsData() {
     return new Promise((resolve, reject) => {
 
@@ -45,4 +67,5 @@ function getAllPlanets() {
 module.exports = {
     loadPLanetsData,
     getAllPlanets,
+    loadPlanetsDataSync
 }
