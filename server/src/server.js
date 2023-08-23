715043b9e0a0946cf53a40a93988c8d20a7538d2
Rupinder <rupinder3972@gmail.com>
2023-08-23 16:06:35 +0530

    added changes for making loading function sync

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const app = require('./app');
-const { loadPLanetsData } = require('./models/planets.model');
+const { loadPlanetsDataSync } = require('./models/planets.model');
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
 async function startServer () {
-    await loadPLanetsData();
+    loadPlanetsDataSync();
 
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
