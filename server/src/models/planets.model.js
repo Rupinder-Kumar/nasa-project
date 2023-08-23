e976f1bc0fe1bda3f74301802a0255fabb8d9180
Rupinder <rupinder3972@gmail.com>
2023-08-23 16:09:43 +0530

    uncommented code

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -11,7 +11,7 @@ function isHabitablePlanet(planet) {
         && planet['koi_prad'] < 1.6;
 }
 
- function  loadPlanetsDataSync() {
+ function loadPlanetsDataSync() {
     fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
         .pipe(parse({
             comment: '#',
@@ -33,32 +33,32 @@ function isHabitablePlanet(planet) {
         });
 }
 
-// function loadPLanetsData() {
-//     return new Promise((resolve, reject) => {
+function loadPLanetsData() {
+    return new Promise((resolve, reject) => {
 
-//     fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
-//         .pipe(parse({
-//             comment: '#',
-//             columns: true,
-//         }))
-//         .on('data', (data) => {
-//             if (isHabitablePlanet(data)) {
-//                 habitablePlanets.push(data);
-//             }
-//         })
-//         .on('error', (err) => {
-//             console.log(err);
-//             reject(err);
-//         })
-//         .on('end', () => {
-//             console.log(habitablePlanets.map((planet) => {
-//                 return planet['kepler_name'];
-//             }));
-//             console.log(`${habitablePlanets.length} habitable planets found!`);
-//             resolve();
-//         });
-//     })
-// }
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true,
+        }))
+        .on('data', (data) => {
+            if (isHabitablePlanet(data)) {
+                habitablePlanets.push(data);
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on('end', () => {
+            console.log(habitablePlanets.map((planet) => {
+                return planet['kepler_name'];
+            }));
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+            resolve();
+        });
+    })
+}
 
 function getAllPlanets() {
     return habitablePlanets;
