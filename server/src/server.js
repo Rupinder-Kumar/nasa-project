a87f21c747f8714bb692d6b756924655cc5c8d70
Rupinder <rupinder3972@gmail.com>
2023-08-28 13:20:15 +0530

    implemented service to fetch spacex launch data and save into mongodb also serve in existing launches api

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,14 @@
 const http = require('http');
 const app = require('./app');
 const { loadPLanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 const { mongoConnect } = require("./services/mongo");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 async function startServer () {
     await mongoConnect();
     await loadPLanetsData();
+    await loadLaunchData();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
     });
